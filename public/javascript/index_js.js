d098923ba8433507d2892328107189aed3fc59c4
Vitor Leonardo <montehey@gmail.com>
2023-03-15 17:02:10 -0300

    Foi melhorado o select dinâmico do formulário de cadastro de alunos, tanto logicamente quanto esteticamente; foi melhorado a lógica do formulário de cadastro de aluno logicamente.

--- public/javascript/index_js.js
+++ public/javascript/index_js.js
@@ -1,49 +1,72 @@
-    /// ; Pega o select#Nivel_escolar
-    let Nivel_escolar = document.getElementById("Nivel_escolar");
-
-    const value_elem_id = { 
-      'Médio':'Turma_medio',
-    /// ^ value   :  ^ id do select turma **
-      'Subsequente':'Turma_sub'
-    };
+      
+      let Nivel_escolar = document.getElementById("Nivel_escolar");
+      Nivel_escolar.onchange = function(){
     
-    /// ; Define o evento onchange no select#Nivel_escolar
-    Nivel_escolar.onchange = function(){
-        /// ; this aqui dentro é o select#Nivel_escolar
-        let valor = this.value;                     /// ; <- valor selecionado
-        let selects = Array.from(                   /// ; <- transforma o resultado em array
-          document.getElementsByTagName('select')   /// ; <- pega todos os selects 
-        );
-        
-        selects.forEach(function(el){
-        
-            if(el.id != 'Nivel_escolar')  /// ; <- se o select não for o de Nivel_escolar display=none
-                el.style.display='none';
-            if(el.id == value_elem_id[valor] )  /// ; <- se o select é o da turma display=block;
-                el.style.display='block';
-                
-        });
-        /// document.getElementById( value_elem_id[valor] ).style.display='block';
-        /// ; outra forma de dar display block      
-    }
+        const value_elem_id = { 
+          'Médio':'Turma_medio',
+          'Subsequente':'Turma_sub'
+        };
+
+          let valor = this.value;                     
+          let selects = Array.from(                   
+            document.getElementsByTagName('select')   
+          );
+          
+          selects.forEach(function(el){
+              if(el.id != 'Nivel_escolar')  
+                  el.style.display='none';
+              if(el.id == value_elem_id[valor] )
+                  el.style.display='block';    
+          });
+      }
     
 
-
-const cadastroAluno = document.querySelector(".form-cadastro");
+const cadastro = document.querySelector(".cadastro-aluno")
+const formCadastroAluno = document.querySelector(".form-cadastro");
 const cadastroAlunoBackground = document.querySelector("#contain-cadastro");
+
+const resetarFormulario = ()=>{
+  cadastro.reset(); // Reseta o formulário;
+  // Volta para o estado normal do select quando o formulario for submetido;
+      let selects = Array.from(document.getElementsByTagName('select'));
+      selects.forEach(function(el){
+        if(el.id != 'Nivel_escolar')  
+           el.style.display='none';
+        });
+}
 
 document.addEventListener('click', function(event){
     event.preventDefault();
     const evento = event.target.className
     console.log(evento)
     if(evento === "cadastrar-aluno"){
-        cadastroAluno.style.display = 'block';
-        cadastroAlunoBackground.classList.add("contain-cadastro");
-        
+      formCadastroAluno.style.display = 'block';
+        cadastroAlunoBackground.classList.add("contain-cadastro");   
     }
-    if(evento === "Cadastrar"){
-        cadastroAluno.style.display = 'none';
-        cadastroAlunoBackground.classList.remove("contain-cadastro")
 
-   }
+    if(evento === "Cadastrar"){
+      const camposObrigatorios = formCadastroAluno.querySelectorAll('[required]');
+    let preenchidos = true;
+
+    camposObrigatorios.forEach(function(campo){
+      if(!campo.value){
+        preenchidos = false;
+      }
+    })
+    
+    if(!preenchidos){
+      event.preventDefault();
+      alert("Preencha todos os campos!")
+    }else{
+      formCadastroAluno.style.display = 'none';
+      cadastroAlunoBackground.classList.remove("contain-cadastro");
+      resetarFormulario();
+    } 
+  }
+
+  if(evento === 'contain-cadastro'){
+    formCadastroAluno.style.display = 'none';
+    cadastroAlunoBackground.classList.remove("contain-cadastro");
+    resetarFormulario();
+  }
 })
