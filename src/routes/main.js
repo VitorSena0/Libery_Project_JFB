b150b644b590757ab1ed097dd404a411a268da26
João Vitor <jv644@protonmail.com>
2023-05-21 10:41:42 -0300

    Adição da leitura dee dados do banco para a listagem dos dados do aluno na página 'Alunos' usando axios

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const axios = require('axios')
 const route = express()
 const AlunoRoutes = {
     main:route.get('/', (req,res) => {
@@ -7,8 +8,16 @@ const AlunoRoutes = {
     Erro404:route.get("/404",(req,res) => {
       res.send("Erro 404")
     }),
-    AlunoMain:route.get('/aluno', (req,res) => {
-        res.render("aluno")
+    AlunoMain:route.get('/aluno', async (req,res) => {
+      try {
+        const response = await axios.get('http://localhost:3000/aluno/ReadAluno');
+        const alunos = response.data; // Supondo que a resposta contenha os dados dos alunos
+
+        res.render("aluno", { alunos });
+      } catch (error) {
+        console.error('Erro na requisição:', error);
+        res.status(500).send('Erro na requisição');
+      }
     }),
     BookMain:route.get('/book', (req,res) => {
         res.render("book")
