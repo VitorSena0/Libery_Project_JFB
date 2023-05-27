4da9076bb10f5c46693ebc00b4632357ba9f9583
João Vitor <jv644@protonmail.com>
2023-05-27 10:47:44 -0300

    HotFix:Correção de Erros na edição de valores da página acervos e no repositorio git

--- public/javascript/livrosCadastrados.js
+++ public/javascript/livrosCadastrados.js
@@ -1,4 +1,9 @@
-// Tabela jquery
+// Configurações da tabela jquery;
+// https://api.jquery.com/ready/
+// https://datatables.net/reference/option/
+// https://api.jquery.com/on/
+// https://datatables.net/reference/api/search()
+
 $(document).ready(function () {
     $('#myTable').DataTable({
         "pagingType": "full_numbers",
@@ -20,7 +25,7 @@ $(document).ready(function () {
 });
 
 
-// Variáveis para o formulário
+
 const modal = document.querySelector('.contain-cadastro');
 const tbody = document.querySelector('tbody');
 const sTitulo = document.querySelector('#titulo');
@@ -28,117 +33,167 @@ const sAutor = document.querySelector('#autor');
 const sEditora = document.querySelector('#editora');
 const sGenero = document.querySelector('#genero');
 const sEstoque = document.querySelector('#estoque');
+const btnSalvar = document.querySelector('.CadastrarLivro')
+let oldtitle = ""
+function openmodal(){
+  sTitulo.value = ""
+  sAutor.value = ""
+  sEditora.value = ""
+  sGenero.value = ""
+  sEstoque.value = ""
+  modal.classList.add('active')
 
-const submit = document.querySelector('.CadastrarLivro');
-
-let itens;
-let id;
-
-// Esta função é chamada quando se clica no botão de adicionar livro;
-function openModal(edit = false, index = 0) {
-    modal.classList.add('active'); // Adiciona este nome para que o formulário mude de display para block
-    modal.onclick = e => {
-        // Esta condicional verificará quando houver um evento de click no formulário e se neste click a pessoa clicar em algum elemento que não contenha tal classe ele removerá o nome active;
-        if (e.target.className.indexOf('contain-cadastro active') !== -1) {
-            modal.classList.remove('active');
-
-        }
-    }
-    // Faz com que quando for clicado para editar um item ele ponha as informações do objeto nos campos do formulário respectivamente para que se possa editá-los
-    if (edit) {
-        sTitulo.value = itens[index].titulo;
-        sAutor.value = itens[index].autor;
-        sEditora.value = itens[index].editora;
-        sGenero.value = itens[index].genero;
-        sEstoque.value = itens[index].estoque;
-        id = index
-        // A atribuição do id para o index evita que quando o formulário ao editado e depois submetido ele não crie um novo elemento/obj ao invés de alterar os dados
-
-    } else {
-        sTitulo.value = '';
-        sAutor.value = '';
-        sEditora.value = '';
-        sGenero.value = '';
-        sEstoque.value = '';
-
-    }
 }
-// Ésta função é chamada quando o botão de editar é clicado
-const editItem = index => openModal(true, index);
-
-const deleteItem = index => {
-    itens.splice(index, 1);
-    setItensBD();
-    loadItens();
-}
-
-// Esta função é chamada quando a função onclick atribuita a submit é chamada
-function insertItem(item, index) {
-    let tr = document.createElement('tr');
-
-    tr.innerHTML = `
-    <td>${item.titulo}</td>
-    <td>${item.autor}</td>
-    <td>${item.editora}</td>
-    <td>${item.genero}</td>
-    <td>${item.estoque}</td>
-    <td class='acao'>
-        <button onclick="editItem(${index})"><img class= "imagem-acao-tabela" src="images/editar.png" title="Editar"></img></button>
-    </td>
-    <td class="acao">
-      <button onclick="deleteItem(${index})"><img class="imagem-acao-tabela" src="images/excluir.png" title="Deletar"></img></button>
-    </td>
-    `
-    tbody.appendChild(tr);
-}
-
-// Quando o formulário é submetido esta função é ativada
-submit.onclick = e => {
-    // Esta condicional impede que algum campo do formulário esteja vazio 
-    if (sTitulo.value === '' || sAutor.value === '' || sEditora.value === '' || sEstoque.value === '' || sGenero.value === '') {
-        return;
-    }
-
-    e.preventDefault(); // Evita que quando o formulário for mandado ele abra uma nova javela
-
-    if (id !== undefined) {
-        itens[id].titulo = sTitulo.value;
-        itens[id].autor = sAutor.value;
-        itens[id].editora = sEditora.value;
-        itens[id].genero = sGenero.value;
-        itens[id].estoque = sEstoque.value;
-    } else {
-        itens.push({ 'titulo': sTitulo.value, 'autor': sAutor.value, 'editora': sEditora.value, 'genero': sGenero.value, 'estoque': sEstoque.value })
-    }
-
-    setItensBD();
-
+let itens = {}
+function modifyElement(pointer,data){
+    console.log(data)
+    console.log(pointer)
+   document.getElementById(pointer + "-titulo").innerText = data.oldtitle;
+   document.getElementById(pointer + "-autor").innerText = data.autor;
+   document.getElementById(pointer + "-editora").innerText = data.editora;
+    document.getElementById(pointer + "-genero").innerText = data.genero;
+    document.getElementById(pointer + "-estoque").innerText = data.estoque;
     modal.classList.remove('active');
-
-    loadItens() // O loadItens é sempre chamado quando se cria um novo obj.
-    id = undefined;
 }
-
-
-const loadItens = () => {
-    itens = getItensBD();
-    tbody.innerHTML = '';
-    itens.forEach((item, index) => {
-        insertItem(item, index)
-        // O index vem da quantidade de itens que tem na tabela, ou seja, quando é chamada a função forEach(), dentro dela tem o intens.
-        //forEach() que pegará todos os elementos e indices(Posição do array em relação a tabela) salvos no banco de dados do browser.
-        // Por isso que a cada elemento o index do 'delet' e do 'edit' estão diferentes a medida que se vai adicionando elementos. 
-    })
+function editItem(pointer){
+  // Encontra o elemento na tabela com base no ponteiro
+  let element = document.getElementById(pointer);
+  let form = document.querySelector(".form-cadastro-livro")
+  id = pointer
+  status = "edit"
+  form.action = "/book/UpdateBook"
+ // Verifica se o elemento existe
+ if (element) {
+   // Obtém os valores dos campos do elemento selecionado
+   let titulo = document.getElementById(pointer + "-titulo").innerText
+   let autor = document.getElementById(pointer + "-autor").innerText;
+   let editora = document.getElementById(pointer + "-editora").innerText;
+   let genero = document.getElementById(pointer + "-genero").innerText
+   let estoque = document.getElementById(pointer + "-estoque").innerText
+   // Preenche os campos do formulário de edição com os valores do item selecionado
+   oldtitle = titulo
+   sTitulo.value = titulo
+   sAutor.value = autor
+   sEditora.value = editora
+   sGenero.value = genero
+   sEstoque.value = estoque
+   // Exibe o modal de edição
+   modal.classList.add('active');
+ }
 }
-
-const getItensBD = () => JSON.parse(localStorage.getItem('dbLivros')) ?? []
-const setItensBD = () => localStorage.setItem('dbLivros', JSON.stringify(itens))
-
-loadItens()
-
+modal.onclick = e => {
+  // Esta condicional verificará quando houver um evento de click no formulário e se neste click a pessoa clicar em algum elemento que não contenha tal classe ele removerá o nome active;
+  if (e.target.className.indexOf('contain-cadastro active') !== -1) {
+    modal.classList.remove('active');
+  }
+};
+function deleteItem(pointer) {
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST', '/book/DeleteBook', true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  let data = {
+    titulo:pointer
+  };
+  var jsonData = JSON.stringify(data);
+  console.log("work it")
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        console.log(xhr.responseText);
+      } else {
+        console.error('Erro na requisição:', xhr.status);
+      }
+    }
+  }
+  xhr.send(jsonData);
+  let table = document.getElementById("data")
+  let element = document.getElementById(`(${pointer})`);
+  table.removeChild(element);
+}
+function insertItem(item,pointer) {
+  let form = document.querySelector(".form-cadastro-livro")
+  form.action = "/book/SignBook"
+  let tr = document.createElement('tr')
+  tr.id = "("+pointer+")"
+  tr.innerHTML = `
+  <td id="(${pointer})-titulo">${item.titulo}</td>
+  <td id="(${pointer})-autor">${item.autor}</td>
+  <td id="(${pointer})-editora">${item.editora}</td>
+  <td id="(${pointer})-genero">${item.genero}</td>
+  <td id="(${pointer})-estoque">${item.estoque}</td>
+  <input type="hidden" value="${pointer}">
+  <td class="acao">
+    <button onclick="editItem('(${pointer})')"><img class="imagem-acao-tabela" src="/public/images/editar.png" title="Editar"></img></button>
+  </td>
+  <td class="acao">
+    <button onclick="deleteItem('${pointer}')"><img class="imagem-acao-tabela" src="/public/images/excluir.png" title="Deletar"></img></button>
+  </td>
+`;
+  tbody.appendChild(tr)
+}
+btnSalvar.onclick = e => {
+  if (sTitulo.value == '' || sAutor.value == '' || sEditora.value == '' || sGenero.value == '' || sEstoque.value == '') {
+    return;
+  }
+  let form = document.querySelector(".form-cadastro-livro")
+  e.preventDefault();
+  var xhr = new XMLHttpRequest();
+  let data = {
+    oldTitle:oldtitle,
+    titulo:sTitulo.value,
+    autor: sAutor.value,
+    genero: sGenero.value,
+    editora: sEditora.value,
+    estoque: sEstoque.value
+  };
+  console.log(data)
+  var jsonData = JSON.stringify(data);
+  if(status == "edit"){
+    xhr.open('POST', '/book/UpdateBook', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    modifyElement(id,data)
+    console.log("work it")
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          itens.oldTitle = sTitulo.value
+          itens.titulo = sTitulo.value
+          itens.autor =  sAutor.value
+          itens.genero =  sGenero.value
+          itens.editora =  sEditora.value
+          itens.estoque =  sEstoque.value
+          console.log(xhr.responseText);
+        } else {
+          console.error('Erro na requisição:', xhr.status);
+        }
+      }
+    }
+    xhr.send(jsonData);
+    modal.classList.remove('active')
+  } else if (status = "add"){
+    xhr.open('POST', '/book/SignBook', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          itens.titulo = sTitulo.value
+          itens.autor =  sAutor.value
+          itens.genero =  sGenero.value
+          itens.editora =  sEditora.value
+          itens.estoque =  sEstoque.value
+          var response = JSON.parse(xhr.responseText);
+          console.log(response);
+          insertItem(itens, response.titulo);
+        } else {
+          console.error('Erro na requisição:', xhr.status);
+        }
+      }
+    };
+        xhr.send(jsonData);
+        modal.classList.remove('active')
+    }
+}
 const atualizarTabela = document.querySelector('.atualizar-tabela');
-
 atualizarTabela.onclick = function () {
   window.location.reload();
 }
-
--- src/routes/book.js
+++ src/routes/book.js
@@ -15,14 +15,72 @@ const BookRoutes = {
           client.release();
       }
     }),
-    AddBook:route.post("/book/SignBook",(req,res) => {
-        //ToDo
+    AddBook:route.post("/book/SignBook",async (req,res) => {
+      const client = await con.connect();
+      const client2 = await con.connect();
+      const reqBody = {
+        titulo:req.body.titulo,
+        autor:req.body.autor,
+        editora:req.body.editora,
+        genero:req.body.genero,
+        estoque:req.body.estoque
+      }
+      console.log(reqBody)
+      try {
+          await client.query(`INSERT INTO tb_books (titulo,autor,editora,genero,estoque) Values ('${reqBody.titulo}','${reqBody.autor}','${reqBody.editora}','${reqBody.genero}','${reqBody.estoque}')` );
+          const result = await client2.query(`SELECT titulo FROM tb_books `);
+          res.send(result.rows[result.rows.length - 1]);
+
+      } catch (error) {
+          console.error('Erro ao inserir dados', error);
+          throw error;
+      } finally {
+          client.release();
+          client2.release();
+      }
     }),
-    RemoveBook:route.post("/book/DeleteBook",(req,res) => {
-        //ToDo
+    RemoveBook:route.post("/book/DeleteBook",async (req,res) => {
+      const client = await con.connect();
+      const reqBody = {
+        titulo:req.body.titulo
+      }
+      console.log(req.body)
+      try {
+          await client.query(`DELETE FROM tb_books WHERE titulo = '${reqBody.titulo}'`);
+          res.send("Dados Deletados com sucesso");
+      } catch (error) {
+          console.error('Erro ao inserir dados', error);
+          throw error;
+      } finally {
+          client.release();
+      }
     }),
-    UpdateBook:route.post("/book/UpdateBook",(req,res) => {
-        //ToDo
+    UpdateBook:route.post("/book/UpdateBook",async (req,res) => {
+      const client = await con.connect();
+      const reqBody = {
+        oldTitle:req.body.oldTitle,
+        titulo:req.body.titulo,
+        autor:req.body.autor,
+        editora:req.body.editora,
+        genero:req.body.genero,
+        estoque:req.body.estoque
+      }
+      console.log(req.body)
+      try {
+          await client.query(`UPDATE tb_books
+                              SET titulo = '${req.body.titulo}',
+                                  autor = '${req.body.autor}',
+                                  editora = '${req.body.editora}',
+                                  genero = '${req.body.genero}',
+                                  estoque = '${req.body.estoque}'
+                              WHERE titulo = '${req.body.oldTitle}'`);
+          res.send("Dados alterados com sucesso");
+      } catch (error) {
+          console.error('Erro ao inserir dados', error);
+          throw error;
+      } finally {
+          client.release();
+      }
     })
 }
 
